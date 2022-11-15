ab4fe42faecb945a55a98d59e4013e74bc46a850
Simon - fantomitechno <fantomitechno@gmail.com>
2022-11-15 11:55:42 +0100

    feat: remove f*ed things

--- src/main/java/dev/renoux/dinnermod/mixins/SkinOptionsScreenMixin.java
+++ src/main/java/dev/renoux/dinnermod/mixins/SkinOptionsScreenMixin.java
@@ -1,13 +1,13 @@
 package dev.renoux.dinnermod.mixins;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ScreenTexts;
+//import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.CyclingButtonWidget;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.Option;
+//import net.minecraft.client.option.Option;
 import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,7 +22,7 @@ public class SkinOptionsScreenMixin extends GameOptionsScreen {
         super(parent, gameOptions, title);
     }
 
-    //@Inject(at = @At("TAIL"), method = "init")
+    // @Inject(at = @At("TAIL"), method = "init")
     /**
      * @author minemobs
      * @reason I'm bad at mixin stuff
@@ -33,28 +33,34 @@ public class SkinOptionsScreenMixin extends GameOptionsScreen {
         PlayerModelPart[] var2 = PlayerModelPart.values();
 
         for (PlayerModelPart playerModelPart : var2) {
-            this.addDrawableChild(CyclingButtonWidget.onOffBuilder(this.gameOptions.isPlayerModelPartEnabled(playerModelPart))
+            this.addDrawableChild(CyclingButtonWidget
+                    .onOffBuilder(this.gameOptions.isPlayerModelPartEnabled(playerModelPart))
                     .build(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20,
-                            playerModelPart.getOptionName(), (button, enabled) -> this.gameOptions.togglePlayerModelPart(playerModelPart, enabled)));
+                            playerModelPart.getOptionName(),
+                            (button, enabled) -> this.gameOptions.togglePlayerModelPart(playerModelPart, enabled)));
             ++i;
         }
 
-        this.addDrawableChild(Option.MAIN_HAND.createButton(this.gameOptions, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150));
+        //this.addDrawableChild(Option.MAIN_HAND.createButton(this.gameOptions, this.width / 2 - 155 + i % 2 * 160,
+        //        this.height / 6 + 24 * (i >> 1), 150));
         ++i;
-        if (i % 2 == 1) ++i;
+        if (i % 2 == 1)
+            ++i;
 
         DPlayerModelPart playerModelPart = DPlayerModelPart.DINNERBONE;
-
+        /*
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 155 + i % 2 * 160,
                 this.height / 6 + 24 * (i >> 1), 150, 20, playerModelPart.getText(),
                 button -> {
                     playerModelPart.addEnabled();
                     button.setMessage(playerModelPart.getText());
                 }));
-
+        
         ++i;
-
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 30 * (i >> 1), 200, 20, ScreenTexts.DONE,
-                (button) -> this.client.setScreen(this.parent)));
+        
+        this.addDrawableChild(
+                new ButtonWidget(this.width / 2 - 100, this.height / 6 + 30 * (i >> 1), 200, 20, ScreenTexts.DONE,
+                        (button) -> this.client.setScreen(this.parent)));
+                         */
     }
 }
